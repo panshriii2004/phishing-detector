b70cebaa4ac3fe73e231910290f1c68bab9f31fc
Panshri <byeetoall@gmail.com>
2026-03-31 16:30:51 +0530

    Added dev server (run.py) and deployment setup

--- app.py
+++ app.py
@@ -31,6 +31,3 @@ def home():
     return render_template("index.html", result=result)
 
 # IMPORTANT: Only run locally
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
